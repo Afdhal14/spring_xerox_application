a38ab07f853cb10655d9f7f3933501ee2788d5bd
Afdhal14 <afdhalofficial1414@gmail.com>
2025-08-07 11:19:50 +0530

    init commit

---
+++ src/main/java/com/web/xeroxApp/XeroxAppApplication.java
@@ -0,0 +1,13 @@
+package com.web.xeroxApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XeroxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XeroxAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/xeroxApp/XeroxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.xeroxApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XeroxAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
